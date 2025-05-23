b503453365ad7cd799e6e2ae09b061aed527c66c
Serj Bakumec <sbakumtsev@yandex.ru>
2025-05-23 09:21:35 +0300

    Base sructure data

---
+++ data_struct.py
@@ -0,0 +1,92 @@
+class Stack:
+    def __init__(self):
+        self.__elements = []
+    
+    def push(self, element):
+        self.__elements.append(element)
+    
+    def pop(self):
+        try:  
+            element = self.__elements.pop()
+        except IndexError:
+            element = None 
+        return element 
+          
+    def get_len_stack(self):
+        return len(self.__elements)
+       
+    # def get_all_stack(self):
+    #     return self.__elements
+    
+
+from collections import deque
+class Queue_1:
+    def __init__(self):
+        self.__elements = deque()
+   
+    def enqueue(self, element):
+        self.__elements.append(element)
+
+    def dequeue(self):
+        try:
+            element = self.__elements.popleft()
+        except IndexError:
+            element = None    
+        return element
+    
+    def get_len_queue(self):
+        return len(self.__elements)
+
+
+class Queue_2:
+    def __init__(self):
+        self.__elements = []
+   
+    def add(self, element):
+        self.__elements.insert(0, element)
+
+    def remove(self):
+        try:
+            element = self.__elements.pop()
+        except IndexError:
+            element = None    
+        return element
+    
+    def get_len_queue(self):
+        return len(self.__elements)
+        
+    # def get_all_queue(self):
+    #     return self.__elements
+
+class Queue_3:
+    def __init__(self):
+        self.__elements = []
+   
+    def add(self, element):
+        self.__elements.append(element)
+
+    def remove(self):
+        try:
+            element = self.__elements.pop(0)
+        except IndexError:
+            element = None    
+        return element
+    
+    def get_len_queue(self):
+        return len(self.__elements)
+        
+    # def get_all_queue(self):
+    #     return self.__elements
+    
+queue = Queue_3()
+queue.add(1)
+queue.add(2)
+
+print(queue.get_len_queue())
+print(queue.get_all_queue())
+
+print(queue.remove())                 
+print(queue.remove())                 
+print(queue.remove())                 
+            
+    
